7c0d368d8e0247306059c1d02213588733c6af3a
Andreas Nyhren <andreas.nyhren@gmail.com>
2012-05-31 17:28:35 +0200

    settings

--- script/settings.js
+++ script/settings.js
@@ -1,9 +1,5 @@
 var Settings = {
 	lastfm:{
 		apiKey:'00198b31b392d0750f88819830e49680'
-	},
-	parse:{
-		appId: '62KNSBKTmxr81a8RieUtxgBiKzjZPXlU612Vkl78',
-		restApiKey: 'AuT5jYsq803QQMRndT9acutY3ZDeB8f6Q5TiVxYV',
 	}
 };
