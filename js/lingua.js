4e54626c42289c56b1712f05cbfacc495a1decf6
Andreas Nyhren <andreas.nyhren@gmail.com>
2013-06-24 14:46:57 +0200

    moved stuff from index.html to lingua.js

--- js/lingua.js
+++ js/lingua.js
@@ -10,11 +10,37 @@ encoding = utf-8
 
  usage: >pybabel extract -F babel.cfg -k _n:1,2 -k _ -o messages.pot . partials js
 
+using poedit the tabs for plural translations does not always appear, see https://drupal.org/node/17564
+
  >npm install -g po2json
  and per locale:
  >po2json translations/sv-se.po l_sv-se.json
  */
 
+ function linguaInit(doc,cb) {
+    var locale = localStorage.getItem('locale');
+    console.log(locale);
+    if (locale) {
+      var s = doc.createElement('script');
+      s.setAttribute('src', "//code.angularjs.org/1.1.5/i18n/angular-locale_"+locale+".js");
+      doc.body.appendChild( s );
+    } else {
+      locale = "en-us";
+    }
+    microAjax('l_'+locale+'.json',function(data) {
+      data = JSON.parse(data);
+      var i18n = new Jed({
+        "domain" : locale,
+        "missing_key_callback" : function(key) {
+            console.error("Missing key " + key + " for " + locale);
+        },
+        locale_data : data
+      });
+      console.log(i18n);
+      window.i18n = i18n;
+      cb();
+    });
+}
 
 angular.module('lingua',[]);
 
@@ -34,7 +60,7 @@ angular.module('lingua').factory('linguaService',function() {
     return linguaService;
 });
 
-angular.module('lingua').controller('linguaController',['$scope', '$window',function LoginCtrl($scope,$window) {
+angular.module('lingua').controller('linguaController',['$scope', '$window',function linguaController($scope,$window) {
     $scope.changeLocale = function(locale) {
         // so, only way to reload $locale is on full page reload
         // and load angular-locale_xx-yy.js
